9584d52cd7d987bf4283d8cf576262897d022a72
oblomov <102328295+oblomov-dev@users.noreply.github.com>
2023-07-16 22:49:59 +0000

    update frontend

---
+++ frontend/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("z2ui5odata.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ frontend/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"z2ui5_odata/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
