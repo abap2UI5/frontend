96da24b99205cfcea5a57cd5286052fca0b3a652
oblomov-dev <102328295+oblomov-dev@users.noreply.github.com>
2025-02-22 09:39:50 -0500

    test-action

--- app/webapp/controller/View1.controller.js
+++ app/webapp/controller/View1.controller.js
@@ -7,6 +7,7 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/core/mvc/XMLView", "sap/ui/
         "use strict";
         return Controller.extend("z2ui5.controller.View1", {
 
+            
             onInit() {
 
                 z2ui5.oRouter.attachRouteMatched(function (oEvent) {
@@ -16,7 +17,7 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/core/mvc/XMLView", "sap/ui/
 
             },
             async onAfterRendering() {
-
+                    
                 if (!z2ui5.oResponse) {
                     return;
                 }
